74d813c9aa9b94e57509d253f0c389455c8f4d6f
Jesus Kahwati <jesuskahwati@MacBook-Pro-de-pc.local>
2022-07-01 03:52:25 -0400

    Add example message

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,74 @@
 import express from 'express';
-const app = express()
-const port = 3000
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
-app.get('/', (req: any, res:any) => {
-  res.send('Hello World!')
-})
+// Construct a schema, using GraphQL schema language
+let schema = buildSchema(`
+  input MessageInput {
+    content: String
+    author: String
+  }
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
+  type Query {
+    getMessage(id: ID!): Message
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
+  }
+`);
+
+// If Message had any complex fields, we'd put them on this object.
+class Message {
+
+  private id : string
+  private content : string
+  private author : string
+
+  constructor(id : any , { content, author } : any) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
+
+// Base de datos
+let fakeDatabase : any = {};
+
+let root = {
+  getMessage: ({ id } : any) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: ({ input } : any) => {
+    // Create a random id for our "database".
+    let id = require('crypto').randomBytes(10).toString('hex');
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: ({ id, input } : any) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+};
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(3000);
+console.log('Running a GraphQL API server at http://localhost:3000/graphql');
